a63b5f108cce6ec8270016104fde8e807de87c57
Godbless Amankwah <g_amankwah@mail.fhsu.edu>
2025-09-25 21:19:27 -0400

    LoanServices added

---
+++ backend/backend/src/main/java/CSCI_841_Project/backend/service/LoanService.java
@@ -0,0 +1,26 @@
+package csci841_project.backend.service;
+
+import csci841_project.backend.dto.LoanDTO;
+import csci841_project.backend.entity.User;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface LoanService {
+    LoanDTO createLoan(LoanDTO loanDTO);
+
+    LoanDTO getLoanById(Long loanId);
+
+    List<LoanDTO> getLoansByUser(Long userId);
+
+    LoanDTO updateLoan(Long loanId, LoanDTO loanDTO);
+
+    void deleteLoan(Long loanId);
+
+    void updateLoanStatus(Long loanId);
+
+    void updateNextDueDate(Long loanId);
+
+    void updateLoanStats(User user);
+
+}
